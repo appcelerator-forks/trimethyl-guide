b19cf8fe391ad4d3ccec97def7bdb7fcf8ca6110
Ani Sinanaj <ani.sinanaj@caffeina.com>
2017-10-02 18:01:38 +0200

    Refactor

--- app/models/configuration.js
+++ app/models/configuration.js
@@ -11,19 +11,17 @@ exports.definition = {
 		_.extend(Model.prototype, {
 			
 			getImageBase: function(size, type) {
-				if (!this.get('images')) return;
 				var img = this.get('images');
-				var url = img.secure_base_url;
+				if (!img || img.length == 0) return;
 				
 				// getting sizes for the image type
 				// and filtering them by the closest 
 				// or return the latest in the list 
-				url += _(img[type + "_sizes"]).filter(function(s) {
+				return img.secure_base_url + _(img[type + "_sizes"]).filter(function(s) {
 					// return true if the absolute value of the 
 					// size - requested size is less than or equal to 20
 					return Math.abs( parseInt( s.substr(1) ) - size ) <= 30;
 				}).join() || img[type + "_sizes"][img[type + "_sizes"].length - 1];
-				return url;
 			}
 
 		}); 
